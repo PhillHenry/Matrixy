c422ade018a985a529da30bf2ac35131762f6d59
Phillip Henry <PhillHenry@gmail.com>
2020-09-28 08:04:57 +0100

    Incompatible matrices fail to compile as expected.

--- src/test/scala/uk/co/odinconsultants/matrixy/MultiplyTest.scala
+++ src/test/scala/uk/co/odinconsultants/matrixy/MultiplyTest.scala
@@ -5,8 +5,14 @@ class MultiplyTest extends munit.FunSuite {
   implicit val multiplier = new MockMultiply
 
   import TestObjects._
+  import Multiply.ops._
 
   test ("multiply compatible matrices") {
-    multiplier.multiply(_3x7)(_7x8)
+    _3x7 x _7x8
   }
+
+  test("incompatible multiplication does not compile") {
+    compileErrors("_3x7 x _8x7")
+  }
+
 }
