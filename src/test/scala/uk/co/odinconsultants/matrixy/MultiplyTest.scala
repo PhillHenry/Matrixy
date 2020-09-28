671f5ac33d235e25d7db7ee0ec86ac7529bfb85d
Phillip Henry <PhillHenry@gmail.com>
2020-09-28 08:15:59 +0100

    Assertion on compilation failure message.

--- src/test/scala/uk/co/odinconsultants/matrixy/MultiplyTest.scala
+++ src/test/scala/uk/co/odinconsultants/matrixy/MultiplyTest.scala
@@ -12,7 +12,18 @@ class MultiplyTest extends munit.FunSuite {
   }
 
   test("incompatible multiplication does not compile") {
-    compileErrors("_3x7 x _8x7")
+    assertNoDiff(
+      compileErrors("_3x7 x _8x7"),
+      """|error:
+         |type mismatch;
+         | found   : uk.co.odinconsultants.matrixy.MockMatrix[uk.co.odinconsultants.matrixy.TestObjects._8,uk.co.odinconsultants.matrixy.TestObjects._7]
+         |    (which expands to)  uk.co.odinconsultants.matrixy.MockMatrix[eu.timepit.refined.api.Refined[Int,eu.timepit.refined.generic.Equal[Int(8)]],eu.timepit.refined.api.Refined[Int,eu.timepit.refined.generic.Equal[Int(7)]]]
+         | required: uk.co.odinconsultants.matrixy.MockMatrix[uk.co.odinconsultants.matrixy.TestObjects._7,?]
+         |    (which expands to)  uk.co.odinconsultants.matrixy.MockMatrix[eu.timepit.refined.api.Refined[Int,eu.timepit.refined.generic.Equal[Int(7)]],?]
+         |_3x7 x _8x7
+         |       ^
+         |""".stripMargin
+    )
   }
 
 }
